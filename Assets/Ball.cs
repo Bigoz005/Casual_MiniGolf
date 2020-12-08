5185cc075f4e3afc87c8dddb855969388667f7e4
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2020-12-08 19:57:23 +0100

    Zaimportowanie wtyczki, klasy obiektow

---
+++ Assets/Ball.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    float weight = 0.5f;
+    public float speed = 0.0f;
+    float bounciness = 0.5f;
+    float height = 0.0f;
+    float metres = 0.0f;
+
+    float x;
+    float y;
+    float z;
+    public Text heightText;
+    public Text metresText;
+    public GameObject hole;
+
+    void Start()
+    {
+        updateTextFields();
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    private void Update()
+    {
+        updateTextFields();
+        if(speed > 0) { 
+            speed -= 1 * Time.deltaTime;
+            z = -speed;
+            y = speed;
+            x = speed * 0.005f;
+        }
+        this.transform.Translate(new Vector3(x, y, z) * Time.deltaTime * 0.5f);
+        height = this.transform.position.y;
+    }
+
+    private void updateTextFields()
+    {
+        heightText.text = height.ToString();
+        metresText.text = metres.ToString();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Capsule")
+        {
+            speed = 5.0f;
+        }
+
+        if (other.gameObject.name == "Hole")
+        {
+            hole.GetComponent<Hole>().isIn = true;
+        }
+    }
+}
---
+++ Assets/Golfer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Golfer : MonoBehaviour
+{
+    Club club;
+    Hole hole;
+    Ball ball;
+    int strikes;
+    int points;
+    float power;
+    public Text strikesText;
+    public Text pointsText;
+
+    void Start()
+    {
+        strikes = 0;
+        points = 0;
+        power = 0.0f;
+        updateTextFields();
+    }
+
+    private void Update()
+    {
+        updateTextFields();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * Time.deltaTime*2);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.back * Time.deltaTime*2);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * Time.deltaTime*2);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * Time.deltaTime*2);
+        }
+    }
+
+    private void updateTextFields()
+    {
+        pointsText.text = points.ToString();
+        strikesText.text = strikes.ToString();
+    }
+
+}
